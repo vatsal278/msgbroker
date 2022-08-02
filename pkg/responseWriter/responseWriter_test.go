ff8596d2bcb6223b65eea60e8d3e95f36300971a
PereRohit <77770177+PereRohit@users.noreply.github.com>
2022-08-02 18:06:57 +0530

    Updated & fixed test case errors.

--- pkg/responseWriter/responseWriter_test.go
+++ pkg/responseWriter/responseWriter_test.go
@@ -12,34 +12,56 @@ import (
 func TestResponseWriter(t *testing.T) {
 	ctrl := gomock.NewController(t)
 	defer ctrl.Finish()
+
+	type args struct {
+		w      http.ResponseWriter
+		status int
+		msg    string
+		data   interface{}
+	}
 	tests := []struct {
-		name             string
-		expectedResponse interface{}
+		name     string
+		input    args
+		mocks    func(*mocks.MockResponse)
+		validate func(http.ResponseWriter, error)
 	}{
 		{
-			name:             "SUCCESS:: ParseRequest",
-			expectedResponse: http.StatusOK,
+			name: "SUCCESS:: ResponseWriter",
+			input: args{
+				w:      httptest.NewRecorder(),
+				status: http.StatusOK,
+				msg:    "OK",
+				data:   "Hello",
+			},
+			mocks: func(controller *mocks.MockResponse) {
+				controller.EXPECT().Update(http.StatusOK, "OK", "Hello").MaxTimes(1)
+			},
+			validate: func(w http.ResponseWriter, err error) {
+				if err != nil {
+					t.Errorf("Error::Want: %v, Got: %v", nil, err.Error())
+				}
+				v, ok := w.(*httptest.ResponseRecorder)
+				if !ok {
+					t.Errorf("ResponseWriter assert failed")
+				}
+				if v.Code != http.StatusOK {
+					t.Errorf("Want: %v, Got: %v", http.StatusOK, v.Code)
+				}
+				if v.Header().Get("Content-Type") != "application/json" {
+					t.Errorf("Content Type Want: %v, Got: %v", "application/json", v.Header().Get("Content-Type"))
+				}
+			},
 		},
 	}
 	for _, tt := range tests {
-		t.Run("TestValidate", func(t *testing.T) {
-			w := httptest.NewRecorder()
-			//r := httptest.
-			//Mock the interface and use them inside it
+		t.Run(tt.name, func(t *testing.T) {
+			// registering the mocks
 			MockResponse := mocks.NewMockResponse(ctrl)
-			MockResponse.EXPECT().Update(http.StatusOK, "Successfully Registered as publisher to the channel", nil)
-			err := ResponseWriter(w, http.StatusOK, "Successfully Registered as publisher to the channel", nil, MockResponse)
-			contentType := w.Header().Get("Content-Type")
-			if contentType != "application/json" {
-				t.Errorf("Want: Content Type as %v, Got: Content Type as %v", nil, err.Error())
-			}
-			if err != nil {
-				t.Errorf("Want: %v, Got: %v", nil, err.Error())
-			}
-			if w.Code != http.StatusOK {
-				t.Errorf("Want: %v, Got: %v", tt.expectedResponse, w.Code)
-			}
-		},
-		)
+			tt.mocks(MockResponse)
+			// function call
+			err := ResponseWriter(tt.input.w, tt.input.status, tt.input.msg, tt.input.data, MockResponse)
+			// validate
+			tt.validate(tt.input.w, err)
+		})
 	}
 }
