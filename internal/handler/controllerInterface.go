88a1dbafd896fb3d88cf2c3f2f0cb3b2a51fe205
vatsal278 <vatsalchaturvedi278@gmail.com>
2022-08-05 15:27:18 +0530

    added go generate command for mocking interfaces

--- internal/handler/controllerInterface.go
+++ internal/handler/controllerInterface.go
@@ -2,6 +2,8 @@ package controllerInterface
 
 import "net/http"
 
+//go:generate mockgen --destination=./../../mocks/internal/mocks/mock_controller.go --package=mocks github.com/vatsal278/msgbroker/internal/handler IController
+
 type IController interface {
 	RegisterPublisher() func(w http.ResponseWriter, r *http.Request)
 	RegisterSubscriber() func(w http.ResponseWriter, r *http.Request)
