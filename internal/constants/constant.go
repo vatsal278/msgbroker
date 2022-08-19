d59925af78896db3e4cb6435d08deb6cd4acde9c
vatsal278 <vatsalchaturvedi278@gmail.com>
2022-08-19 10:40:48 +0530

    Updated the constants package

--- internal/constants/constant.go
+++ internal/constants/constant.go
@@ -1,6 +1,15 @@
 package constants
 
 const (
-	ParseErr       = "Error parsing request"
-	IncompleteData = "Incomplete data"
+	ParseErr               = "Error parsing request"
+	IncompleteData         = "Error :: Incomplete data"
+	PublisherRegistration  = "Successfully Registered as publisher to the channel"
+	SubscriberRegistration = "Successfully Registered as subscriber to the channel"
+	InvalidUUID            = "Error :: Invalid Universal UID"
+	PublisherNotFound      = "Error :: No publisher found with the specified name for specified channel"
+	NotifiedSub            = "Notified All Subscriber"
+	NoRoute                = "Error :: No Route Found"
+	ConnectedServer        = "Successfully connected to the server"
+	StringUnmarshalError   = "cannot unmarshal string into Go struct field"
+	ValidatorFail          = "failed on the 'required' tag"
 )
